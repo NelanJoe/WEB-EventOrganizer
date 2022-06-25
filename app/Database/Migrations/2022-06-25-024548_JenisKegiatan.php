1414a16c55165a2017425d8e4cdcba005f6a7ec7
Baihaki Al Biruni <baihakitanjung12@gmail.com>
2022-06-25 14:48:01 +0700

    fix: type data

--- app/Database/Migrations/2022-06-25-024548_JenisKegiatan.php
+++ app/Database/Migrations/2022-06-25-024548_JenisKegiatan.php
@@ -17,7 +17,8 @@ class JenisKegiatan extends Migration
                 'auto_increment' => true
             ],
             'nama'       => [
-                'type'           => 'DATE'
+                'type'           => 'VARCHAR',
+                'constraint'     => 100,
             ],
             'created_at DATETIME DEFAULT CURRENT_TIMESTAMP',
             'updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
--- app/Database/Migrations/2022-06-25-030650_KategoriPeserta.php
+++ app/Database/Migrations/2022-06-25-030650_KategoriPeserta.php
@@ -17,7 +17,8 @@ class KategoriPeserta extends Migration
                 'auto_increment' => true
             ],
             'nama'       => [
-                'type'           => 'DATE'
+                'type'           => 'VARCHAR',
+                'constraint'     => 100,
             ],
             'created_at DATETIME DEFAULT CURRENT_TIMESTAMP',
             'updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
