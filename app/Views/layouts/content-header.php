7dd466cd84215a06140c1c430319a6cfa68850be
Nelan <49295389+NelanJoe@users.noreply.github.com>
2022-07-10 20:56:17 +0700

    fix:conflict

--- app/Views/layouts/content-header.php
+++ app/Views/layouts/content-header.php
@@ -3,13 +3,13 @@
      <div class="container-fluid">
          <div class="row mb-2">
              <div class="col-sm-6">
-                 <h1 class="m-0"> <?= str_replace('-', ' ', strtoupper(explode('/cms', current_url())[4]));  ?></h1>
+                 <h1 class="m-0"> <?= str_replace('-', ' ', strtoupper(explode('/', current_url())[4]));  ?></h1>
              </div>
              <div class="col-sm-6">
                  <ol class="breadcrumb float-sm-right">
                      <li class="breadcrumb-item"><a href="<?= base_url('/'); ?>">Home</a></li>
                      <li class="breadcrumb-item active">
-                         <?php $url = str_replace('-', ' ', explode('/cms', current_url())[4]) ?>
+                         <?php $url = str_replace('-', ' ', explode('/', current_url())[4]) ?>
                          <?= ucwords($url, " \t\r\n\f\v'") ?>
                      </li>
                  </ol>
